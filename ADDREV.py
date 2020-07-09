46fe1832d71490dd7238155785d2d15acb5f143a
Ashish Khulbey <65804342+ashish-khulbey@users.noreply.github.com>
2020-07-09 22:08:10 +0530

    Update ADDREV.py

--- ADDREV.py
+++ ADDREV.py
@@ -3,9 +3,7 @@
 t = int(input())
 while t>0:
 	a,b = input().split()
-	# print(a, b)
 	a = int(a[::-1])  # Reverse first number
 	b = int(b[::-1])  # Reverse second number
 	print(int(str((a+b))[::-1]))  # Add and print reversed ans
-	# print(a+b)
 	t=t-1
